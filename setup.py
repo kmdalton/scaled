a69e6bdef50361f0752ef3b3fa725c44d49bf94d
Kevin <kmdalton@ishmael.(none)>
2013-10-26 16:26:35 -0700

    Setup no compiles the C-code
     Committer: Kevin <kmdalton@stanford.edu>
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 ###############################################################################
 
 from ftplib import FTP
+from subprocess import call
 
 
 #Download the nr database from ncbi
@@ -12,6 +13,11 @@ def downloadNR(dbFilename):
     ftp.cwd('blast/db/FASTA')
     ftp.retrbinary('RETR nr.gz', open(dbFilename, 'wb').write)
 
+def compileCLibs():
+    call(['gcc', '-std=c99', '-fPIC', '-shared', '-fopenmp', '-o', 'joint.so', 'joint.c'])
+    call(['gcc', '-std=c99', '-fPIC', '-shared', '-fopenmp', '-o', 'pinf.so', 'pinf.c'])
+
 
 if __name__=="__main__":
+    compileCLibs()
     downloadNR('nr.gz')
