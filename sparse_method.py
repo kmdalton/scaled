7420dfb74052a7cd5634ea019f1c59bd4c425276
Kevin Dalton <kmdalton@stanford.edu>
2014-11-29 12:53:48 -0800

    New library to solve convex problem
     Changes to be committed:
    	new file:   sparse_method.py
              This library contains a single function which uses cvxpy in order to solve
              a convex optimization problem which reweights marginal & joint entropies
              in order to yield a sparse normalized mutual information matrix. I think
              this will be very useful in the future.
              sparse_method depends on cvxpy (http://cvxpy.readthedocs.org/en/latest/index.html)

---
+++ sparse_method.py
@@ -0,0 +1,26 @@
+import pinfwrapper,fullmsa
+import numpy as np
+import cvxpy as cvx
+
+def l1_least_squares(J, **kw): 
+    #Empirical parameters
+    L= np.shape(J)[0]
+    W  = cvx.Variable(L, L)
+    alpha = kw.get('alpha', 1.)
+    rho = kw.get('rho', 0.5) #Ratio of l2
+    verbose= kw.get('verbose', False)
+    Lidentity = np.identity(L)
+
+    #alpha --> controls sparsity of 1 - pinfwrapper.infoDistance(mtx)
+    constraints = [W == W.T, W >= 0., cvx.hstack(*(cvx.diag(W) for i in range(L))) + cvx.hstack(*(cvx.diag(W) for i in range(L))).T - W >= 0.]
+    p = cvx.Problem(cvx.Minimize(
+                        rho*cvx.sum_squares(np.diag(J) - cvx.diag(W))/float(L) +
+                        (1. - rho)*cvx.sum_squares(J - W)/float(L*L) +
+                        alpha*cvx.norm(cvx.hstack(*(cvx.diag(W) for i in range(L))) + cvx.hstack(*(cvx.diag(W) for i in range(L))).T - Lidentity - W, 1)
+                    ), constraints)
+
+    p.solve(solver=kw.get('solver', 'SCS'), verbose=kw.get('verbose', False), max_iters=kw.get('max_iters', 25000))
+    if kw.get('verbose', False) == True:
+        print "\n\nSolver finished up with status: {}".format(p.status)
+    W = np.array(p.variables()[0].value)
+    return W
