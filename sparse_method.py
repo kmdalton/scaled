dd254792a0d6d5f083333c75903abd802907f05e
Kevin Dalton <kmdalton@stanford.edu>
2015-05-12 17:09:44 -0700

    added sparse matrix sqrt to sparse_method

--- sparse_method.py
+++ sparse_method.py
@@ -1,4 +1,5 @@
 import pinfwrapper,fullmsa
+import scipy.linalg
 import numpy as np
 import cvxpy as cvx
 
@@ -19,17 +20,40 @@ def l2_max_entropy(mtx, **kw):
     p = cvx.Problem(cvx.Maximize((1./float(M))*cvx.sum_entries(cvx.entr(weights.T*masks)) - rho*cvx.norm2(weights)), constraints)
     return p
 
-def fix_positive_definiteness(C):
-    """
-    Some extreme weightings on the normalized mutual information lead to the accumulation
-    of numerical errors culminating in a single negative eigenvalue. As such, I've written
-    this filter to remove the nullspace from the eigenspectrum by increasing all the eigen
-    values to be >=0.
-    """
+def columnwise_max_weights(mtx, **kw):
+    rho = kw.get('rho', 0.)
+    P = [l2_max_entropy(i, rho=rho) for i in mtx.T]
+    for p in P:
+        p.solve(solver='SCS')
+    W = np.array([np.array(p.variables()[0].value).flatten() for p in P])
+    return W
 
-    v,vec = np.linalg.eig(C)
-    if v.min() < 0.:
-        v = v - v.min()
-        return np.matrix(vec)*np.matrix(np.diag(v))*np.matrix(vec).T
-    else:
-        return C
+def iter_sparse_sqrt(mtx, **kw):
+    alpha = kw.get('alpha', .001)
+    iters = kw.get('iters', 10)
+    threshold = kw.get('threshold', 1.)
+    solver_kwargs = kw.get('solver_kwargs', {'solver': 'SCS', 'verbose': True, 'max_iters': 100000})
+    C = 1. - pinfwrapper.infoDistance(mtx)
+    R = scipy.linalg.sqrtm(C)
+    for i in range(iters):
+        p = sparse_sqrt(C, R, alpha=alpha)
+        p.solve(**solver_kwargs)
+        T = np.array(p.variables()[0].value)
+        if np.sum(np.square(R-T)) < threshold:
+            R = T.copy()
+            break
+        R = T.copy()
+    return R
+
+def sparse_sqrt(C, R, **kw):
+    L,L = np.shape(C)
+    alpha = kw.get('alpha', .001)
+    I = np.matrix(np.linalg.inv(R))*np.matrix(C)
+    R = cvx.Variable(L,L)
+    constraints = [R == R.T]
+    p = cvx.Problem(
+        cvx.Minimize((1./L/L)*cvx.sum_squares(R - I) + alpha*(1./L/L)*cvx.norm1(R)), 
+    constraints)
+    return p
+
+
