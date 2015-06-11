4eb1fd7f068d096fe6a6f47b8926cace0adab55b
Kevin Dalton <kmdalton@stanford.edu>
2015-06-11 12:41:23 -0700

    added a maximum entropy relaxation model 	modified:   sparse_method.py

--- sparse_method.py
+++ sparse_method.py
@@ -3,9 +3,19 @@ import scipy.linalg
 import numpy as np
 import cvxpy as cvx
 
+def jpd(mtx):
+    M,L = np.shape(mtx)
+    k = mtx.max()+1
+    bivariate_mapper = np.arange(k*k).reshape((k,k))
+    j = bivariate_mapper[mtx[:,:,None], mtx[:,None,:]]
+    j = j.reshape(M, L**2)
+    j = np.vstack([np.bincount(i, minlength=k**2)/float(M) for i in j.T])
+    return j.reshape((k**2, L, L))
+
+
 def max_entropy(mtx, **kw):
     M = np.shape(mtx)[0]
-    masks = cvx.hstack(*(cvx.Constant(mtx==i) for i in range(mtx.max()+1)))
+    masks = cvx.hstack(*(cvx.Constant(mtx==i) for i in np.where(np.bincount(mtx.flatten()) > 0)[0]))
     weights= cvx.Variable(M)
     constraints=[weights >= 0., cvx.sum_entries(weights) == 1.]
     p = cvx.Problem(cvx.Maximize(cvx.sum_entries(cvx.entr(weights.T*masks))), constraints)
@@ -22,9 +32,10 @@ def l2_max_entropy(mtx, **kw):
 
 def columnwise_max_weights(mtx, **kw):
     rho = kw.get('rho', 0.)
-    P = [l2_max_entropy(i, rho=rho) for i in mtx.T]
+    P = [max_entropy(i) for i in mtx.T]
     for p in P:
-        p.solve(solver='SCS')
+        p.solve(solver='SCS', max_iters=100000)
+        p.solve(max_iters=100000)
     W = np.array([np.array(p.variables()[0].value).flatten() for p in P])
     return W
 
@@ -62,7 +73,32 @@ import rpy2.robjects.numpy2ri
 from rpy2.robjects.packages import importr
 rpy2.robjects.numpy2ri.activate()
 
-def glasso(cov, rho):
+def glasso(cov, rho, glasso_kwargs = None):
     dpglasso = importr('dpglasso')
     return np.array(dpglasso.dpglasso(cov, rho=rho)[2])
 
+def MPD(mtx):
+    M,L = np.shape(mtx)
+    k = mtx.max() + 1
+    return np.vstack((np.histogram(i, k, (-.01,20.1))[0]/float(M) for i in mtx.T))
+    
+
+def MER(mtx, **kw):
+    delta = kw.get('delta', 0.01)
+    k = mtx.max() + 1
+    M,L = np.shape(mtx)
+    JPD = jpd(mtx)
+    X,Y = np.triu_indices(L, 1)
+    JPD = [i[i > 0.] for i in JPD[:,X,Y].T]
+    print np.shape(JPD)
+    print np.shape(JPD[0])
+    V = [cvx.Variable(len(i)) for i in JPD]
+    constraints = []
+    H = 0.
+    for j,v in zip(JPD,V):
+        constraints.append(sum([cvx.kl_div(p1,p2) for p1,p2 in zip(j,v)]) <= delta)
+        constraints.append(v >= 0.)
+        H += cvx.sum_entries(cvx.entr(v))
+    print type(H)
+    print [type(i) for i in constraints[:10]]
+    return cvx.Problem(cvx.Maximize(H), constraints)
