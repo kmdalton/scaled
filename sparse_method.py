e8e731c41537e8abb6e75cedb68a12d0feaed49e
Kevin Dalton <kmdalton@stanford.edu>
2015-11-03 11:51:19 -0800

    minimize mutual information in sparse_method 	modified:   sparse_method.py         -class minmi is very similar to class entropy

--- sparse_method.py
+++ sparse_method.py
@@ -3,6 +3,7 @@ from multiprocessing import Pool,cpu_count
 import numpy as np
 import cvxpy as cvx
 from scipy import sparse
+from time import time
 
 class entropy():
     def __init__(self, mtx):
@@ -41,16 +42,23 @@ class entropy():
             T.append(W)
         return H,T
 
+    def nullentropy(self, **kw):
+        w = kw.get('weights', sparse.csr_matrix(np.ones(self.M)/float(self.M)))
+        J = (w*self.mask).T*(w*self.mask)
+        J.data = J.data*np.log2(J.data)
+        return J
+
     def __call__(self, w, **kw):
         columns = kw.get('columns', np.arange(self.L*self.k))
         W = sparse.csr_matrix(np.diag(w))
         O = self.mask[:,columns].T*W*self.mask
         return np.nansum(O.data*np.log(O.data))
 
-def project(W):
+def project(W, **kw):
+    bound = kw.get('bound', 0.) #Bound for the simplex
     M = len(W)
     V = cvx.Variable(M)
-    p = cvx.Problem(cvx.Minimize(cvx.norm2(V-W)), [cvx.sum_entries(V) == 1., V >=0.])
+    p = cvx.Problem(cvx.Minimize(cvx.norm2(V-W)), [cvx.sum_entries(V) == 1., V >= bound])
     try:
         p.solve()
     except:
@@ -73,3 +81,73 @@ class grad_helper():
         w[i,i] = 1.
         o = self.A[:,self.columns].T*w*self.A
         return -(self.O.multiply(o).sum())
+
+class minmi():
+    def __init__(self, mtx):
+        self.mtx = mtx.copy()
+        self.mask= self.get_sparse_mask()
+        self.M, self.L = mtx.shape
+        self.k = mtx.max()+1
+        self._nonzero_elems = self.mask.T*self.mask
+        self.pool = Pool(cpu_count())
+
+    def get_sparse_mask(self):
+        M,L = np.shape(self.mtx)
+        k = self.mtx.max()+1
+        mask = sparse.lil_matrix((M,L*k))
+        for i in range(k):
+            mask[:, np.arange(L)*k+i] = np.array(self.mtx==i, dtype=float)
+        return sparse.csr_matrix(mask)
+
+    def gradient(self, W, **kw):
+        M = self.mask.shape[0]
+        helper = grad_helper(self.mask, W)
+        grad = np.array(self.pool.map(helper, np.arange(M)))
+        return grad
+
+    def gradient_descent(self, **kw):
+        verbose = kw.get('verbose', False)
+        bound = kw.get('bound', 0.)
+        W = kw.get('wo', np.ones(self.M)/float(self.M))
+        alpha = kw.get('alpha', 1e-7)
+        maxiter = kw.get('maxiter', 100)
+        H,T = [],[]
+        start = time()
+        for i in range(maxiter):
+            if verbose:
+                print "Entering gradient descent cycle {}/{}".format(i+1, maxiter)
+            W = W - alpha*self.gradient(W)
+            W = project(W, bound=bound)
+            H.append(self(W))
+            T.append(W)
+            if verbose:
+                print "\tCycle {} complete, objective = {}".format(i+1, H[-1])
+                print "\t{} s elapsed".format(time() - start)
+        return H,T
+
+    def __call__(self, w):
+        W = sparse.csr_matrix(np.diag(w))
+        w = sparse.csr_matrix(w)
+        O = self.mask.T*W*self.mask
+        E = w*self.mask
+        LogArg = O.T.multiply(E.power(-1)).T.multiply(E.power(-1))
+        LogArg.data = np.log(LogArg.data)
+        return (O.multiply(LogArg)).sum()
+
+
+class minmi_helper():
+    def __init__(self, A, W, **kw):
+        self.A = sparse.csr_matrix(A)
+        self.M = sparse.csr_matrix(W)*A
+        self.W = sparse.csr_matrix(np.diag(W))
+        self.J = self.A.T*self.W*self.A
+
+        self.I = self.J.copy()
+        self.I = self.I.T.multiply(self.E.power(-1)).T.multiply(self.E.power(-1))
+        self.I.data = np.log(self.I.data) + 1.
+
+    def __call__(self, i):
+        Jn = self.A[i].T*self.A[i]
+        M = self.A[i].multiply(self.M.power(-1))
+        return (Jn.multiply(self.I) - self.J.T.multiply(M).T.multiply(M)).sum()
+
