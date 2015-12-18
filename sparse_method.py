fixed minimum muinf bug 	modified:   sparse_method.py     --The gradient descent algorithm for minimizing mutual information     was using the wrong helper function. I believe it is mostly fixed now,     but it is not thoroughly tested yet.

sparse_method.py
@@ -11,7 +11,6 @@ class entropy():
         self.mask= self.get_sparse_mask()
         self.M, self.L = mtx.shape
         self.k = mtx.max()+1
-        self._nonzero_elems = self.mask.T*self.mask
         self.pool = Pool(cpu_count())
 
     def get_sparse_mask(self):
@@ -55,14 +54,15 @@ class entropy():
         return np.nansum(O.data*np.log(O.data))
 
 def project(W, **kw):
-    bound = kw.get('bound', 0.) #Bound for the simplex
+    verbose = kw.get('verbose', False)
+    bound = kw.get('bound', 0.) 
     M = len(W)
     V = cvx.Variable(M)
-    p = cvx.Problem(cvx.Minimize(cvx.norm2(V-W)), [cvx.sum_entries(V) == 1., V >= bound])
+    p = cvx.Problem(cvx.Minimize(cvx.norm2(V-W)), [cvx.sum_entries(V) == 1., V > bound])
     try:
-        p.solve()
+        p.solve(max_iters=100000, verbose=verbose)
     except:
-        p.solve(solver="SCS")
+        p.solve(solver="SCS", max_iters=100000, verbose=verbose)
 
     return np.array(p.variables()[0].value).flatten()
 
@@ -83,17 +83,18 @@ class grad_helper():
         return -(self.O.multiply(o).sum())
 
 class minmi():
-    def __init__(self, mtx):
+    def __init__(self, mtx, **kw):
         self.mtx = mtx.copy()
-        self.mask= self.get_sparse_mask()
+        self.mask= self.get_sparse_mask(nogaps=kw.get('nogaps', False))
         self.M, self.L = mtx.shape
         self.k = mtx.max()+1
-        self._nonzero_elems = self.mask.T*self.mask
         self.pool = Pool(cpu_count())
 
-    def get_sparse_mask(self):
+    def get_sparse_mask(self, **kw):
         M,L = np.shape(self.mtx)
         k = self.mtx.max()+1
+        if kw.get('nogaps', False):
+            k = k - 1
         mask = sparse.lil_matrix((M,L*k))
         for i in range(k):
             mask[:, np.arange(L)*k+i] = np.array(self.mtx==i, dtype=float)
@@ -101,7 +102,7 @@ class minmi():
 
     def gradient(self, W, **kw):
         M = self.mask.shape[0]
-        helper = grad_helper(self.mask, W)
+        helper = minmi_helper(self.mask, W)
         grad = np.array(self.pool.map(helper, np.arange(M)))
         return grad
 
@@ -118,6 +119,8 @@ class minmi():
                 print "Entering gradient descent cycle {}/{}".format(i+1, maxiter)
             W = W - alpha*self.gradient(W)
             W = project(W, bound=bound)
+            if verbose:
+                print "W is: {}".format(W)
             H.append(self(W))
             T.append(W)
             if verbose:
@@ -143,11 +146,14 @@ class minmi_helper():
         self.J = self.A.T*self.W*self.A
 
         self.I = self.J.copy()
-        self.I = self.I.T.multiply(self.E.power(-1)).T.multiply(self.E.power(-1))
+        self.I = self.I.T.multiply(self.M.power(-1)).T.multiply(self.M.power(-1))
         self.I.data = np.log(self.I.data) + 1.
 
     def __call__(self, i):
         Jn = self.A[i].T*self.A[i]
         M = self.A[i].multiply(self.M.power(-1))
-        return (Jn.multiply(self.I) - self.J.T.multiply(M).T.multiply(M)).sum()
+        return (Jn.multiply(self.I) - self.J.multiply(M) - self.J.T.multiply(M).T).sum()
+
+
+
 